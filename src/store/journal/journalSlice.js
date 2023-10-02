14fb900297cf1af30bfc16a9c5b625966e7b406a
josebrusa <josebrusa@outlook.com>
2023-10-03 00:07:18 +0200

    configurando para los pasos de un CRUD

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const journalSlice = createSlice({
+    name: "journal",
+    initialState: {
+        isSaving: true,
+        messageSaved: "",
+        notes: [],
+        active: null,
+        // active: {
+        //       id: "ABC123",
+        //       title: "",
+        //       body: "",
+        //       date: 1234567,
+        //       imageUrls: [],
+        //   },
+    },
+    reducers: {
+        addNewEmptyNote: (state, action) => {},
+        satActiveNote: (state, action) => {},
+        setNotes: (state, action) => {},
+        setSaving: (state) => {},
+        updateNote: (state, action) => {},
+        deleteNoteById: (state, action) => {},
+    },
+});
+// Action creators are generated for each case reducer function
+export const {
+    addNewEmptyNote,
+    satActiveNote,
+    setNotes,
+    setSaving,
+    updateNote,
+    deleteNoteById,
+} = journalSlice.actions;
