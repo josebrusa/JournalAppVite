41487f382638290a78187f84f4d66d2802ff43d3
josebrusa <josebrusa@outlook.com>
2023-10-06 13:24:21 +0200

    implementando sweetAlert

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,16 +1,20 @@
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
-import { ImageGallery } from "../components";
 import { useForm } from "../../hooks/useForm";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useMemo } from "react";
-import { useEffect } from "react";
-import { satActiveNote } from "../../store/journal";
-import { startSavingNote } from "../../store/journal";
+import { SaveOutlined } from "@mui/icons-material";
+import { satActiveNote, startSavingNote } from "../../store/journal";
+import { ImageGallery } from "../components";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2";
 
 export const NoteView = () => {
     const dispatch = useDispatch();
-    const { active: note } = useSelector((state) => state.journal);
+    const {
+        active: note,
+        messageSaved,
+        isSaving,
+    } = useSelector((state) => state.journal);
 
     const { body, title, date, onInputChange, formState } = useForm(note);
     const dateString = useMemo(() => {
@@ -21,6 +25,12 @@ export const NoteView = () => {
     useEffect(() => {
         dispatch(satActiveNote(formState));
     }, [formState]);
+
+    useEffect(() => {
+        if (messageSaved.length > 0) {
+            Swal.fire("Nota Actualizada", messageSaved, "success");
+        }
+    }, [messageSaved]);
 
     const onSaveNote = () => {
         dispatch(startSavingNote());
@@ -42,6 +52,7 @@ export const NoteView = () => {
             </Grid>
             <Grid item>
                 <Button
+                    disabled={isSaving}
                     onClick={onSaveNote}
                     color="primary"
                     sx={{ padding: 2 }}
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -25,13 +25,14 @@ export const journalSlice = createSlice({
         },
         satActiveNote: (state, action) => {
             state.active = action.payload;
+            state.messageSaved = "";
         },
         setNotes: (state, action) => {
             state.notes = action.payload;
         },
         setSaving: (state) => {
             state.isSaving = true;
-            //TODO: mensaje de error
+            state.messageSaved = "";
         },
         updateNote: (state, action) => {
             state.isSaving = false;
@@ -41,7 +42,10 @@ export const journalSlice = createSlice({
                 }
                 return note;
             });
+
+            state.messageSaved = `${action.payload.title}, actualizada correctamente`;
         },
+
         deleteNoteById: (state, action) => {},
     },
 });
